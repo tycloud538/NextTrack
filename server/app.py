66a53e9424e649074029fb6dd416778a475c1483
Tony Han <tyh835cloud@gmail.com>
2025-05-31 23:02:39 -0700

    process json

--- server/app.py
+++ server/app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return "Welcome to NextTrack. Your goto place for music recommendations."
 
-@app.route("/track/recommendation", methods=['POST', 'GET'])
+
+@app.route("/track/recommendation", methods=["POST", "GET"])
 def track_recommendation():
-    return { "recommendation": "Your next track is: Not like us - Kendrick Lamar" }
+    recommendation = {
+        "recommendation": "Your next track is: Not like us - Kendrick Lamar"
+    }
+
+    if request.method == "POST":
+        return {**request.json, **recommendation}
+    return recommendation
