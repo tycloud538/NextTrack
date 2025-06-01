652cee03588ac32c3b7650185bcefbdbfbe02c79
Tony Han <tyh835cloud@gmail.com>
2025-05-31 23:04:40 -0700

    add wsgi

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route("/")
 def hello_world():
