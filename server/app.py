087c94e7ff915b1a9cd58fc7bc694e8cf2e04c0a
Tony Han <tyh835cloud@gmail.com>
2025-05-31 23:32:16 -0700

    separate recommendation

--- server/app.py
+++ server/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+from recommendation import get_recommendation
+
 app = Flask(__name__)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
+
 
 @app.route("/")
 def hello_world():
@@ -12,9 +15,7 @@ def hello_world():
 
 @app.route("/track/recommendation", methods=["POST", "GET"])
 def track_recommendation():
-    recommendation = {
-        "recommendation": "Your next track is: Not like us - Kendrick Lamar"
-    }
+    recommendation = get_recommendation(request.json)
 
     if request.method == "POST":
         return {**request.json, **recommendation}
---
+++ server/recommendation.py
@@ -0,0 +1,2 @@
+def get_recommendation(_params):
+    return {"recommendation": "Your next track is: Not like us - Kendrick Lamar"}
