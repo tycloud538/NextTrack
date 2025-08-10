71398e2a9cf49ec62d19eb3b3b5274a602eee918
Tony Han <tyh835cloud@gmail.com>
2025-08-09 17:16:51 -0700

    tags endpoint

--- server/next_track/search.py
+++ server/next_track/search.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request
+from sqlalchemy import func, select
+from sqlalchemy.orm import joinedload
 
+from next_track.db import db
 from next_track.models import Tag, Recording, ArtistCredit
 
 search = Blueprint("search", __name__)
@@ -7,9 +10,22 @@ search = Blueprint("search", __name__)
 
 @search.route("/tags")
 def tags():
-    search_term = request.args.get("search", "")
+    search = request.args.get("search", "").strip()
+    query = db.session.query(Tag)
 
-    return request.args
+    # perform full-text search if search term is provided
+    if search:
+        search_terms = " & ".join([f"{term}:*" for term in search.split()])
+        query = query.where(
+            func.to_tsvector("english", Tag.name).op("@@")(
+                func.to_tsquery(search_terms, postgresql_regconfig="english")
+            )
+        )
+
+    # return tags ordered by rank
+    tags = query.order_by(Tag.rank.desc()).limit(100)
+
+    return {"tags": [{"id": tag.id, "name": tag.name} for tag in tags]}
 
 
 @search.route("/tracks")
