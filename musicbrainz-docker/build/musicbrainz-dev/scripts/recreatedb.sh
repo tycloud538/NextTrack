c7f85d42ea01e73373ebe51beee17e7b44b6abab
Tony Han <tyh835cloud@gmail.com>
2025-09-10 22:47:47 -0700

    remove unused docker

--- musicbrainz-docker/build/musicbrainz-dev/scripts/recreatedb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-dockerize -wait "tcp://${MUSICBRAINZ_POSTGRES_SERVER}:5432" -timeout 60s sleep 0
-psql postgres -U musicbrainz -h "${MUSICBRAINZ_POSTGRES_SERVER}" -c "DROP DATABASE IF EXISTS musicbrainz_db;"; createdb.sh "$@"
